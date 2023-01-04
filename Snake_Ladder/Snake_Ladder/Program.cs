6b73ba7ee27db8de0ff8cd7f77462ea0cde195b3
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-04 21:41:14 +0530

    [Chiru] Start Single Player with Position

---
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Snake_Ladder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Snake Ladder Game");
+            UC1_GamePlay Player = new UC1_GamePlay();
+
+
+
+        }
+
+       
+    }
+}
